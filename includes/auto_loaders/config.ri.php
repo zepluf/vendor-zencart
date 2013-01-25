04730f8c08117f0d8e9370e9b8c8886caa719a47
Raine Nguyen <vunguyen@rubikin.com>
2013-01-26 03:20:53 +0700

    add some missing files

---
+++ includes/auto_loaders/config.ri.php
@@ -0,0 +1,31 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$container->get("plugin")->loadPlugins($container);
+
+// some global vars to be used on Zencart as well
+$request = Request::createFromGlobals();
+
+$core_event = $container->get('storebundle.core_event');
+
+$view = $container->get("view");
+
+// We need to modify the default load order of Zen. Language class must be loaded first
+$autoLoadConfig[80][] = array('autoType'=>'init_script', 'loadFile'=> 'init_languages.php');
+foreach ($autoLoadConfig[110] as $key => $value){
+    if($value['loadFile'] == 'init_languages.php'){
+        unset($autoLoadConfig[110][$key]);
+        break;
+    }
+}
+
+$autoLoadConfig[80][] = array(
+    'autoType' => 'require',
+    'loadFile' => $container->getParameter('kernel.root_dir') . '/frontend_routing.php'
+);
+
+$autoLoadConfig[200][] = array(
+    'autoType' => 'require',
+    'loadFile' => $container->getParameter('kernel.root_dir') . '/init_includes.php'
+);
---
+++ includes/auto_loaders/paypal_ipn.ri.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$container->get("plugin")->loadPlugins($container);
+
+// some global vars to be used on Zencart as well
+$request = Request::createFromGlobals();
+
+$core_event = $container->get('storebundle.core_event');
